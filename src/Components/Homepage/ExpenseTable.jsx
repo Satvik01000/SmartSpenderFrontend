64705c95b1ae196b2b1d3c8246464aa4b5a0e719
Satvik Mishra <satvik0100@Satviks-MacBook-Air-2.local>
2025-02-12 11:35:34 +0530

    Most Expensive Purchase and Current Balance added

--- src/Components/Homepage/ExpenseTable.jsx
+++ src/Components/Homepage/ExpenseTable.jsx
@@ -1,30 +1,31 @@
 import React from "react";
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Typography } from "@mui/material";
+import "../../index.css"
 
 const ExpenseTable = ({ expenses }) => {
     return (
         <>
-            <Typography sx={{ ml: 3, mt: 1, fontSize: 20, fontFamily: "Inter" }}>
+            <Typography sx={{ ml: 3, mt: 1, fontSize: 24, fontFamily: "Diagond" }}>
                 Recent Expenses
             </Typography>
             <TableContainer component={Paper} sx={{ backgroundColor: "#2e2e2e" }}>
                 <Table>
                     <TableHead>
                         <TableRow>
-                            <TableCell sx={{ color: "black", backgroundColor: "yellow" }}>Date</TableCell>
-                            <TableCell sx={{ color: "black", backgroundColor: "yellow" }}>Amount (₹)</TableCell>
-                            <TableCell sx={{ color: "black", backgroundColor: "yellow" }}>Category</TableCell>
-                            <TableCell sx={{ color: "black", backgroundColor: "yellow" }}>Where</TableCell>
+                            <TableCell sx={{ color: "black", backgroundColor: "yellow", fontFamily: "Diagond", fontSize:23 }}>Date</TableCell>
+                            <TableCell sx={{ color: "black", backgroundColor: "yellow", fontFamily: "Diagond", fontSize:23 }}>Amount (₹)</TableCell>
+                            <TableCell sx={{ color: "black", backgroundColor: "yellow", fontFamily: "Diagond", fontSize:23 }}>Category</TableCell>
+                            <TableCell sx={{ color: "black", backgroundColor: "yellow", fontFamily: "Diagond", fontSize:23 }}>Where</TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
                         {expenses.length > 0 ? (
                             expenses.map((expense, index) => (
                                 <TableRow key={index}>
-                                    <TableCell sx={{ color: "white" }}>{new Date(expense.date).toLocaleDateString()}</TableCell>
-                                    <TableCell sx={{ color: expense.type === "debited" ? "#ff5d5d" : "green" }}>{expense.amount}</TableCell>
-                                    <TableCell sx={{ color: "white" }}>{expense.category.title}</TableCell>
-                                    <TableCell sx={{ color: "white" }}>{expense.spentWhere}</TableCell>
+                                    <TableCell sx={{ fontSize:16, color: "white", fontFamily: "Diagond" }}>{new Date(expense.date).toLocaleDateString()}</TableCell>
+                                    <TableCell sx={{ fontSize:16, color: expense.type === "debited" ? "#ff5d5d" : "green", fontFamily: "Diagond" }}>{expense.amount}</TableCell>
+                                    <TableCell sx={{ fontSize:16, color: "white", fontFamily: "Diagond" }}>{expense.category.title}</TableCell>
+                                    <TableCell sx={{ fontSize:16, color: "white", fontFamily: "Diagond" }}>{expense.spentWhere}</TableCell>
                                 </TableRow>
                             ))
                         ) : (
